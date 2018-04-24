9c38cd8ffae1995fad5545697172f5cd9c60393b
AnthonyGW <anthony.waithaka@andela.com>
2018-04-24 19:36:22 +0300

    Update: export the finished build AMI ID to TF_VAR env var

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,20 +4,20 @@ set -eo pipefail
 
 function validate_packer_images {
   # validate frontend image script
-  packer validate -var-file=./packer/variables.json ./packer/frontend.json
+  packer validate -var-file="./packer/variables.json" ./packer/frontend.json
 
   # validate catalogue image script
-  packer validate -var-file=./packer/variables.json ./packer/catalogue.json
+  packer validate -var-file="./packer/variables.json" ./packer/catalogue.json
 }
 
 function build_ami_with_packer {
-  # build amazon machine image for frontend service and output info to frontend_packer.txt
+  # build amazon machine image for frontend service and output the AMI ID to env var
   packer build -var-file="./packer/variables.json" ./packer/frontend.json
+  export TF_VAR_ami_frontend=$(jq '.builds | .[-1].artifact_id' ./packer/outputs/frontend-manifest.json | sed -e 's/us-east-1://g')
 
-  # build amazon machine image for catalogue service and output info to catalogue_out.txt
-  packer build  -var-file="./packer/variables.json" ./packer/catalogue.json 
-  #2&gt;&amp;1 | sudo tee ./packer/outputs/catalogue_out.txt
-  #"owners": ["520217310580"],
+  # build amazon machine image for catalogue service and output the AMI ID to env var
+  packer build  -var-file="./packer/variables.json" ./packer/catalogue.json
+  export TF_VAR_ami_catalogue=$(jq '.builds | .[-1].artifact_id' ./packer/outputs/catalogue-manifest.json | sed -e 's/us-east-1://g')
 
 }
 
