af18fdf08640a00ea2c4a210f2639b4c4bdc4fed
AnthonyGW <anthony.waithaka@andela.com>
2018-04-27 12:42:29 +0300

    Update add comments

--- bash_example.sh
+++ bash_example.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
+set -eo pipefail
+
 function create_directory {
+  echo "Making HOME/sample_dir"
   mkdir -p ~/sample_dir
 }
 
 function create_sample_file {
+  echo "Making HOME/sample_dir/sample.txt"
   touch ~/sample_dir/sample.txt
 }
 
 function copy_sample_file {
+  echo "Copying HOME/sample_dir/sample.txt"
   cp ~/sample_dir/sample.txt ~/sample_dir/copy_sample.txt
 }
 
 function list_directory_content {
-  ls ~/sample_dir/sample.txt
+  echo "Content of sample_dir:"
+  ls ~/sample_dir
 }
 
 function piping_input {
   export some_input="one two three"
   echo $some_input > ~/sample_dir/sample.txt
-  # replace spaces with new lines 
+
+  echo "Replace spaces with new lines"
   cat ~/sample_dir/sample.txt | sed -E -e 's/[[:blank:]]+/\
 /g'
+
+  echo "Text in sample.txt"
   cat ~/sample_dir/sample.txt
 }
 
 create_directory
 create_sample_file
 copy_sample_file
+list_directory_content
 piping_input
